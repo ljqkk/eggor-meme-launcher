944839a03f7da9d60d16446d0a8456814af1406c
ljqkk <xsfhqi@qq.com>
2025-03-11 18:51:00 +0800

    feat: page index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -101,7 +101,21 @@
   padding-left: 24px;
 
   &-item {
+    width: 734px;
+    height: 633px;
+    border-radius: 12px;
+    border: 1px solid #e5e7eb;
+    padding: 25px;
+    margin-top: 24px;
     .top {
+      display: flex;
+      justify-content: space-between;
+      .top-left {
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        gap: 10px;
+      }
       .avatar {
         width: 48px;
         height: 48px;
@@ -123,7 +137,12 @@
         display: flex;
         justify-content: start;
       }
-
+      .right {
+        img {
+          width: 20px;
+          height: 20px;
+        }
+      }
       .name {
         font-weight: bold;
         font-size: 16px;
@@ -135,7 +154,10 @@
 
       .time {
         display: flex;
-
+        margin-left: 5px;
+        justify-content: center;
+        align-items: center;
+        gap: 6px;
         img {
           width: 9px;
           // height: 24px;
@@ -147,6 +169,34 @@
         img {
           width: 100%;
           height: 100%;
+        }
+      }
+    }
+    .center {
+      .center-content {
+        margin: 18px 0;
+      }
+      .image {
+        width: 734px;
+        height: 490px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+    .bottom {
+      display: flex;
+      gap: 20px;
+      margin-top: 20px;
+      .bottom-item {
+        display: flex;
+        img {
+          width: 20px;
+          height: 20px;
+        }
+        .number {
+          margin-left: 5px;
         }
       }
     }
@@ -192,20 +242,24 @@
     <div class="swipper">
       <div class="swipper-item">
         <div class="top">
-          <div class="avatar">
-            <img src="../assets/images/avatar.png" alt="" />
-          </div>
-          <div class="top-content">
-            <div class="name">Sarah Chen</div>
-            <div class="tick">
-              <img src="../assets/images/tick.png" alt="" />
+          <div class="top-left">
+            <div class="avatar">
+              <img src="../assets/images/avatar.png" alt="" />
             </div>
-          </div>
-          <div class="top-right">
-            <div class="desc">@sarahchen</div>
-            <div class="time">
-              <img src="../assets/images/point.png" alt="" />
-              <div>2小时前</div>
+            <div class="item-desc">
+              <div class="top-content">
+                <div class="name">Sarah Chen</div>
+                <div class="tick">
+                  <img src="../assets/images/tick.png" alt="" />
+                </div>
+              </div>
+              <div class="top-right">
+                <div class="desc">@sarahchen</div>
+                <div class="time">
+                  <img src="../assets/images/point.png" alt="" />
+                  <div>2小时前</div>
+                </div>
+              </div>
             </div>
           </div>
           <div class="right">
@@ -213,7 +267,7 @@
           </div>
         </div>
         <div class="center">
-          <div class="content">
+          <div class="center-content">
             很高兴能和大家分享我的新项目！这是我们团队努力的成果，希望得到大家的支持和建议。
           </div>
           <div class="image">
@@ -221,9 +275,19 @@
           </div>
         </div>
         <div class="bottom">
-          <div class="bottom-item" v-for="item in numberlist">
-            <img :src="item.img" alt="" />
-            <div class="number">{{ item.content }}</div>
+          <div class="bottom-item">
+            <img src="../assets/images/like.png" alt="" />
+            <div class="number">1.2K</div>
+          </div>
+          <div class="bottom-item">
+            <div class="icon">
+              <img src="../assets/images/comment.png" alt="" />
+            </div>
+            <div class="number">{{ 286 }}</div>
+          </div>
+          <div class="bottom-item">
+            <img src="../assets/images/share.png" alt="" />
+            <div class="number">分享</div>
           </div>
         </div>
       </div>
@@ -231,19 +295,4 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-const numberlist = ref([
-  {
-    img: '../assets/images/like.png',
-    content: '1.2K',
-  },
-  {
-    img: '../assets/images/comment.png',
-    content: '286',
-  },
-  {
-    img: '../assets/images/share.png',
-    content: '分享',
-  },
-]);
-</script>
+<script lang="ts" setup></script>
